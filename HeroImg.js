b4c923af8f5e5c0669e0cdae8ad498517a3ccbfd
jwilliams140 <jwilliams140@csub.edu>
2026-03-03 11:01:18 -0800

    Placeholder text, buttons, and routing added

--- HeroImg.js
+++ HeroImg.js
@@ -2,15 +2,31 @@ import "./HeroImgStyles.css";
 
 import React from "react";
 import Placeholder from "../assets/placeholder.jpg";
+import { Link } from "react-router-dom";
 
 
 const HeroImg = () => {
-  return <div className = "hero">
+  return ( 
+  <div className = "hero">
     <div className = "mask">
         <img className = "into-img" src = 
-        {Placeholder} alt = "Placeholder"/>
+        {Placeholder} alt = "Placeholder" />
     </div>
-  </div>;
+    <div className = "content">
+      <p> Placeholder Text </p>
+      <h1> Placeholder Text </h1>
+      <div> 
+        <Link to = "/project"
+        className = "btn">Projects</
+        Link>
+        <Link to = "/contact"
+        className = "btn btn-light">
+          Contact
+          </Link>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default HeroImg
