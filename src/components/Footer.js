175c71a06e500f10a7a7ad6fe0c618b8a485ee29
jwilliams140 <jwilliams140@csub.edu>
2026-03-09 16:49:00 -0700

    Imported school icon on home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,22 @@
+import "./FooterStyles.css";
+import React from "react";
+import { FaSchool } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className = "footer">
+        <div className = "footer-cotainer">
+            <div className = "left">
+                <div className = "location">
+                    <FaSchool />
+                </div>
+            </div>
+
+            <div className = "right"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
 
