0b254a68a1ceeb72e5db4db98304dbeb18522cbe
jwilliams140 <jwilliams140@csub.edu>
2026-03-10 14:53:17 -0700

    Fixed paragraph not showing

---
+++ src/components/HeroImg2.js
@@ -0,0 +1,15 @@
+import "./HeroImg2Styles.css";
+import React from "react";
+
+const HeroImg2 = () => {
+  return (
+   <div className = "hero-img">
+    <div className = "heading">
+        <h1> Placeholder Text</h1>
+        <p> Placeholder Paragraph</p>
+    </div>
+  </div>
+  );
+};
+
+export default HeroImg2;
