09ea3f33ed2c2cc02738411937fbec030daa180b
jwilliams140 <jwilliams140@csub.edu>
2026-03-09 17:16:26 -0700

    More icons and information added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import "./FooterStyles.css";
 import React from "react";
-import { FaSchool, FaPhone, FaMailBulk } from "react-icons/fa";
+import { FaSchool, FaPhone, FaMailBulk, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -18,21 +18,39 @@ const Footer = () => {
                 <div className = "phone"> 
                     <h4> <FaPhone size = {20} style = {{ color:
                     "#fff", marginRight: "2rem" }} />
-                    (661) 952-5000 </h4>
+                    (661) 952-5000 
+                    </h4>
                 </div>
                 <div className = "email"> 
-                    <h4> <FaMailBulk size = {20} style = {{ color:
-                    "#fff", marginRight: "2rem" }} />
-                    jwilliams140@csub.edu </h4>
+                    <h4> 
+                    <FaMailBulk size = {20} style = {{ color:
+                    "#fff", marginRight: "2rem" }} 
+                    />
+                    jwilliams140@csub.edu 
+                    </h4>
                 </div>
             </div>
-
             <div className = "right"></div>
+            <h4> About Me </h4>
+            <p> I am a computer science student who enjoys hardware based concepts. </p>
+            <div className = "social"> 
+                    <FaLinkedin 
+                    size = {20} 
+                    style = {{ color: "#fff", marginRight: 
+                        "2rem" }} 
+                    />
+                    <FaGithub 
+                    size = {20} 
+                    style = {{ color: "#fff", marginRight: 
+                        "2rem" }} 
+                    />
+            </div>
         </div>
     </div>
   )
 }
 
 export default Footer
+
 
 
