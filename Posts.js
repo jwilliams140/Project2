3a7036d61de93ed9debf3ba2d098520faf9475f0
jwilliams140 <jwilliams140@csub.edu>
2026-03-10 19:08:14 -0700

    Posts page made with header and footer

---
+++ Posts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg2 from "../components/HeroImg2";
+
+const Posts = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroImg2 heading = "POSTS"/>
+      <Footer /> 
+    </div>
+  );
+};
+
+export default Posts;
