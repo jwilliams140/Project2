61d41b35e2ef118c9854c05413d60ea7f021b9d7
jwilliams140 <jwilliams140@csub.edu>
2026-03-10 19:11:10 -0700

    Resume page made with header and footer

---
+++ Resume.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg2 from "../components/HeroImg2";
+
+const Resume = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroImg2 heading = "RESUME"/>
+      <Footer /> 
+    </div>
+  );
+};
+
+export default Resume;
