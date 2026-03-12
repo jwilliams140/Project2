38ac6ead88ff4e555ef068c7c3ab204b63eea0c1
jwilliams140 <jwilliams140@csub.edu>
2026-03-12 16:58:00 -0700

    Blog one slug data created

--- Posts.js
+++ Posts.js
@@ -1,16 +1,14 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import HeroImg2 from "../components/HeroImg2";
+export const posts = [
+  {
+    id: 1,
+    title: "My Opinion on ESP32 Smart Clock",
+    date: "March 12, 2026",
+    slug: "ESP32-Clock",
+    excerpt: "How I discovered a project and why I wanted to give it a try.",
+    content: `I read an article on using a ESP32 embedded board to make a smart clock.
+    
+    It was so interesting I decided to share it with my peers in our secret discord.
 
-const Posts = () => {
-  return (
-    <div>
-      <Navbar />
-      <HeroImg2 heading = "POSTS"/>
-      <Footer /> 
-    </div>
-  );
-};
-
-export default Posts;
+    After thinking about it, I would like to give it a try and see what I can accomplish`,
+  }
+]
