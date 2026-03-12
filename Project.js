0032c6d73c449f1394572bf0fe629b9cd3b23b4d
jwilliams140 <jwilliams140@csub.edu>
2026-03-11 17:10:27 -0700

    Imports and slug data imported

--- Project.js
+++ Project.js
@@ -1,17 +1,42 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import HeroImg2 from "../components/HeroImg2";
+import { useParams } from "react-router-dom";
+import { projects } from "./Projects";
 
 const Project = () => {
+
+  const { slug } = useParams();
+
+  const project = projects.find((p) => p.slug === slug);
+
+  if (!project) {
+    return <h1>Project Not Found</h1>;
+  }
+
   return (
     <div>
-      <Navbar/>
-      <HeroImg2 heading = "PROJECTS"/>
-      <Footer/>
-    </div>
-  )
-}
 
-export default Project
+      <h1>{project.title}</h1>
+
+      <img src={project.image} alt={project.title} />
+
+      <p>{project.description}</p>
+
+      <h3>Tech Stack</h3>
+      <ul>
+        {project.tech.map((techItem, index) => (
+          <li key={index}>{techItem}</li>
+        ))}
+      </ul>
+
+      {project.github && (
+        <a href={project.github} target="_blank" rel="noopener noreferrer">
+          View GitHub Repository
+        </a>
+      )}
+
+    </div>
+  );
+};
+
+export default Project;
+
 
