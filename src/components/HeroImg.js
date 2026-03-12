3f60d83bb007bf1bc30a6c98ad36743e0456cfb1
jwilliams140 <jwilliams140@csub.edu>
2026-03-11 17:17:54 -0700

    Removed unnecessary buttons

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,28 +1,19 @@
 import "./HeroImgStyles.css";
-
 import React from "react";
 import Placeholder from "../assets/placeholder.jpg";
-import { Link } from "react-router-dom";
 
 
 const HeroImg = () => {
   return ( 
   <div className = "hero">
     <div className = "mask">
-        <img className = "into-img" src = 
+        <img className = "placeholder" src = 
         {Placeholder} alt = "Placeholder" />
     </div>
     <div className = "content">
       <p> Placeholder Text </p>
       <h1> Placeholder Text </h1>
       <div> 
-        <Link to = "/project"
-        className = "btn">Projects</
-        Link>
-        <Link to = "/contact"
-        className = "btn btn-light">
-          Contact
-          </Link>
       </div>
     </div>
   </div>
@@ -30,3 +21,4 @@ const HeroImg = () => {
 };
 
 export default HeroImg
+
