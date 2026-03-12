273c01e59a888424b4f5940d1b31096e752215a5
jwilliams140 <jwilliams140@csub.edu>
2026-03-11 17:09:22 -0700

    First project set up in file

--- Projects.js
+++ Projects.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import ESP32 from "../assets/ESP32.jpg";
 
-const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
-
-export default Projects
+export const projects = [
+  {
+    slug: "iot-project",
+    title: "IoT Project",
+    description: "An embedded system project aimed at protecting vulnerable animals from predators",
+    image: ESP32,
+    tech: ["Arduino IDE", "Wokwi", "ESP32"],
+    github: "https://github.com/jebentley90-tech/esp32_security_system_iotproject"
+  }
+];
