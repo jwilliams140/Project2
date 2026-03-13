458288b7bbc90926ee7762fdbd07c595d2c03e1e
jwilliams140 <jwilliams140@csub.edu>
2026-03-12 18:00:53 -0700

    Resume information created

--- Resume.js
+++ Resume.js
@@ -2,15 +2,72 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from "../components/HeroImg2";
+import "../components/ResumeStyles.css";
 
 const Resume = () => {
   return (
     <div>
       <Navbar />
-      <HeroImg2 heading = "RESUME"/>
-      <Footer /> 
+
+      <HeroImg2 heading="RESUME" />
+
+      <div className="resume-container">
+
+        <section>
+          <h2>Professional Summary</h2>
+          <p>
+            Motivated univeristy student with experience in computer troubleshooting, creating programs
+            from different programming languages, and building applications through React. Passionate
+            about learning new tools and taking on a leadership role when collaborating with others.
+          </p>
+        </section>
+
+        <section>
+          <h2>Technical Skills</h2>
+          <ul>
+            <li>Java</li>
+            <li>Assembly Language</li>
+            <li>Computer Troubleshooting</li>
+            <li>React</li>
+            <li>Embedded Systems (IoT)</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Education</h2>
+          <p>
+            Coursework towards Associate's Degree 
+            Antelope Valley College (Attempted)
+
+            Bachelor of Science in Computer Science  
+            California State University, Bakersfield (In Progress)
+          </p>
+        </section>
+
+        <section>
+          <h2>Experience</h2>
+
+          <h3>Portfolio Website/App</h3>
+          <p>
+            Created a portfolio, pokemon search, and introduction app using React with dynamic routing,
+            structured project data, and reusable components for application
+            development class
+          </p>
+
+          <h3>Blog</h3>
+          <p>
+            Created a blog using React Router with dynamic slug
+            routing and posts stored in structured data files.
+          </p>
+
+        </section>
+
+      </div>
+
+      <Footer />
     </div>
   );
 };
 
 export default Resume;
+
