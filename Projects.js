9b1232f399b0beecb9dc0c62f0d49ad248642f39
jwilliams140 <jwilliams140@csub.edu>
2026-03-14 18:19:17 -0700

    Two more projects added to portfolio page

--- Projects.js
+++ Projects.js
@@ -1,12 +1,30 @@
 import ESP32 from "../assets/ESP32.jpg";
+import Piano from "../assets/piano.jpg";
+import Clock from "../assets/esp32clock.jpg";
 
 export const projects = [
   {
     slug: "iot-project",
-    title: "IoT Project",
+    title: "Vulnerable Animal Protection System",
     description: "An embedded system project aimed at protecting vulnerable animals from predators",
     image: ESP32,
     tech: ["Arduino IDE", "Wokwi", "ESP32"],
     github: "https://github.com/jebentley90-tech/esp32_security_system_iotproject"
+  },
+
+  {
+    slug: "iot-project2",
+    title: "IoT Piano Implementation",
+    description: "A personal embedded system meant to relaxing and improve IoT skills.",
+    image: Piano,
+    tech: ["Arduino IDE", "Wokwi", "ESP32"],
+  },
+
+    {
+    slug: "iot-project3",
+    title: "IoT Smart Clock",
+    description: "This project is about taking a ESP32 embedded system and experimenting with how to make it function as a clock.",
+    image: Clock,
+    tech: ["Arduino IDE", "Wokwi", "ESP32"],
   }
 ];
