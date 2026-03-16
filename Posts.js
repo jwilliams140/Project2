4440ff9658d43a49ffef1d686eceae13c1f17a75
jwilliams140 <jwilliams140@csub.edu>
2026-03-15 18:26:06 -0700

    All blog posts completed

--- Posts.js
+++ Posts.js
@@ -1,14 +1,77 @@
 export const posts = [
   {
     id: 1,
-    title: "My Opinion on ESP32 Smart Clock",
+    title: "ESP32 Smart Clock Overview",
     date: "March 12, 2026",
     slug: "ESP32-Clock",
     excerpt: "How I discovered a project and why I wanted to give it a try.",
-    content: `I read an article on using a ESP32 embedded board to make a smart clock.
+    content: `In our secret discord, I shared a link to an article on builing a ESP32 smart clock for $15.
     
-    It was so interesting I decided to share it with my peers in our secret discord.
+    I shared this article with our discord because I thought it was interesting that it's possible to do this.
 
-    After thinking about it, I would like to give it a try and see what I can accomplish`,
+    I hadn't thought about it before but it sounded so good I wish this was our final project for Special Topics.
+    
+    The author Adam Conway didn't use wokwi simulation to test out his code before using it on official hardware.
+    
+    All he needed was a ESP32 development board, a set of four LED dot matrix modules on single PCB, and all of powered
+    from a usb-c cable.
+    
+    He also 3D printed special hardware to house everything but I don't plan on doing that.
+    
+    I plan to deviate away from what the author was doing with his ESP32 smart clock.
+    
+    Conway used ESP32Home, an open source firmware framework where you can configure ESP32 device using YAML file.
+
+    The only time I plan on using ESP32Home is as a last resort if using wokwi simulation prove to be not beneficial for this project.
+
+    The wiring shouldn't be difficult to get it powered up and ready to use but this will be the last step for me.
+
+    I believe testing it out in a simulator and getting it to run should be the major step before deploying to real hardware
+
+    Here is the link to the author's article: https://www.xda-developers.com/built-15-esp32-smart-clock/
+    
+    `,
+  },
+
+    {
+    id: 2,
+    title: "ESP32 Vulnerable Animal Protection System Overview",
+    date: "March 13, 2026",
+    slug: "ESP32-VAP",
+    excerpt: "Why I decided to tackle the topic of vulnerable animals and their predators.",
+    content: `Our special topics final project inspired me to try to implement a temporary fix to vulnerable animal dying from attacks.
+    
+    My family and I have parakeets a pets. We have gone through a few because there is a certain hawk that comes around every winter.
+
+    Most of the time they have not died because it attacked them but it seems like they get really scared and it is not easy on their heart.
+    
+    In our special topics class, I partnered up with Joseph Bentley and we decided we wanted to tackle this problem.
+    
+    We have brainstormed on how we should approach this and what we believe is reasonable approach to dealing with the hawk while protecting the parakeets.
+    
+    So far we theorized using buzzers as an electrical acuator and and DC motor as a mechanical actuator to scare the hawk away.
+    
+    The noises and swinging of the motor should top the hawk from killing the parakeets.
+    
+    We were also concerned about the parakeet because they can easily scare so we plan to have the volume at a reasonable level.
+    
+    There is so much more we can do in our project but we will continue to assess the possibilities as the semester goes on.`,
+  },
+
+      {
+    id: 3,
+    title: "Portfolio Experience Review",
+    date: "March 13, 2026",
+    slug: "React-Portfolio",
+    excerpt: "My opinion on this portfolio and why I believe it's the best we've done so far.",
+    content: `I just wanted to make this into a blog post because so far I believe this is the best project we have done so far.
+    
+    The first lab wasn't as in-depth as the portfolio but this project helped everyone to expand on some thing discussed in the first lab.
+
+    The pokemon lab is not directly related to the portfolio but I think it is ambitious as that lab.
+    
+    To summarize, I believe all labs has helped up until this point.
+    
+    Honetly, I wasn't able to implement much of the stretch goals but it would've been nice to showcase the power of react developmental tools.`,
   }
 ]
